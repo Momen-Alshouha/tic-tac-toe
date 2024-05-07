fbe59d86aa5d087911061adef9e17d4b112f8e1e
momen-alshouha <momenalshouha@outlook.com>
2024-05-07 18:21:05 +0300

    game data design added to the form

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.Title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlayerTurn = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Title
@@ -43,12 +48,73 @@
             this.Title.TabIndex = 0;
             this.Title.Text = "Tic-Tac-Toe";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(815, 279);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 44);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Turn : ";
+            // 
+            // lblPlayerTurn
+            // 
+            this.lblPlayerTurn.AutoSize = true;
+            this.lblPlayerTurn.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayerTurn.ForeColor = System.Drawing.Color.White;
+            this.lblPlayerTurn.Location = new System.Drawing.Point(933, 280);
+            this.lblPlayerTurn.Name = "lblPlayerTurn";
+            this.lblPlayerTurn.Size = new System.Drawing.Size(147, 44);
+            this.lblPlayerTurn.TabIndex = 2;
+            this.lblPlayerTurn.Text = "Player 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(754, 356);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 44);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Winner :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(912, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(199, 44);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "In Progress";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Microsoft Tai Le", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(863, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Restart Game";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // TicTacToe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(1127, 765);
+            this.ClientSize = new System.Drawing.Size(1150, 765);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblPlayerTurn);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Title);
             this.Name = "TicTacToe";
             this.Text = "Form1";
@@ -62,6 +128,11 @@
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlayerTurn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
