511682b4421865dc2e969890312cb292a6e003ed
momen-alshouha <momenalshouha@outlook.com>
2024-05-07 17:43:04 +0300

    vertical and horizantal lines done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace tic_tac_toe
 {
-    partial class Form1
+    partial class TicTacToe
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Tai Le", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.ForeColor = System.Drawing.Color.White;
+            this.Title.Location = new System.Drawing.Point(454, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(259, 55);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "Tic-Tac-Toe";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1127, 765);
+            this.Controls.Add(this.Title);
+            this.Name = "TicTacToe";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.TicTacToe_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label Title;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,30 @@ using System.Windows.Forms;
 
 namespace tic_tac_toe
 {
-    public partial class Form1 : Form
+    public partial class TicTacToe : Form
     {
-        public Form1()
+        public TicTacToe()
         {
             InitializeComponent();
         }
+
+        private void TicTacToe_Paint(object sender, PaintEventArgs e)
+        {
+            Color white = Color.FromArgb(255, 255, 255, 255);
+            Pen pen = new Pen(white);
+            pen.Width = 10;
+
+            pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            // Draw vertical lines
+            e.Graphics.DrawLine(pen, 170, 100, 170, 550);
+            e.Graphics.DrawLine(pen, 350, 100, 350, 550);
+
+            // Draw horizontal lines
+            e.Graphics.DrawLine(pen, 50, 230, 470, 230);
+            e.Graphics.DrawLine(pen, 50, 400, 470, 400);
+        }
+
     }
 }
